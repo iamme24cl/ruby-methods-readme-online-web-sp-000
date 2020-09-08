bb1197d500bdfaf48468aa9fe461f128d9a91084
Chandra Lama <iamme24cl@gmail.com>
2020-09-08 06:23:40 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,10 +1,11 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
+
 
 
 def print_ten_times
-   puts phrase * 10
+  phrase = "Hello World!"
+  puts phrase * 10
 end
 
 print_ten_times
